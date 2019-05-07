a8e44ce0c05a4f599731cfe84810525e8767adfb
Jake Nelken <jake.nelken@datadoghq.com>
2019-05-07 15:53:55 -0400

    Add better attempts to mount. Add col empty search

--- trello-toggle-extension.js
+++ trello-toggle-extension.js
@@ -1,31 +1,78 @@
 window.document.body.onload = function() {
   if (!window.location.pathname.includes("/b/")) return;
 
+  const UNMATCHABLE = "!@%$)^!@@#$)(&";
+
   const HIDE = "display:none;";
   const SHOW = "";
+  let searchText = UNMATCHABLE;
   let nextStyle = HIDE;
-  const button = document.createElement("div");
 
-  button.style =
-    "display:inline-block;margin-right:5px;cursor:pointer;text-align:center;font-size:2em;line-height:1em;padding-left:10px;padding-right:10px;";
+  const button = document.createElement("div");
+  // style it like the other buttons using the Trello class
   button.classList.add("board-header-btn");
+
+  button.style =`
+    display:inline-block;
+    margin-right:5px;
+    cursor:pointer;
+    text-align:center;
+    font-size:2em;
+    line-height:1em;
+    padding-left:10px;
+    padding-right:10px;
+  `.replace(/\s/g, "");
 
   button.onclick = e => {
     let columns = document.querySelectorAll(".js-list.list-wrapper");
     columns.forEach(col => {
-      const hiddenCount = col.querySelectorAll(".list-card.hide").length;
-      const allCount = col.querySelectorAll(".list-card").length;
-      if (hiddenCount === allCount) {
+      const cardsTotalCount = col.querySelectorAll(".list-card").length;
+      const cardsHiddenCount = col.querySelectorAll(".list-card.hide").length;
+      const isEmpty = cardsTotalCount === cardsHiddenCount
+      const headerText = col.querySelector(
+        ".list-header-name-assist.js-list-name-assist"
+      ).innerHTML;
+      const matchesSearch = headerText.includes(searchText);
+
+      if (isEmpty) {
         col.style = nextStyle;
       }
+      // override hide style if matches search
+      if (matchesSearch) {
+        col.style = SHOW;
+      }
     });
+    // toggle style for next button press
     nextStyle = nextStyle === HIDE ? SHOW : HIDE;
   };
 
   button.innerHTML = "☺︎";
 
-  setTimeout(() => {
+  const search = document.createElement("input");
+  search.type = "text";
+  search.style = "width:120px;margin:8px;";
+  search.addEventListener("keyup", e => {
+    searchText = e.target.value || UNMATCHABLE;
+  });
+
+  const component = document.createElement("div");
+  component.style = "display:inline-flex;";
+  component.classList.add("board-header-btn");
+  component.appendChild(search);
+  component.appendChild(button);
+
+  let attempts = 0
+  const interval = setInterval(() => {
+    console.log('attempts', attempts)
     const container = document.querySelector(".board-header-btns.mod-right");
-    container.insertBefore(button, container.firstChild);
-  }, 3000);
+    if(container) {
+      container.insertBefore(component, container.firstChild);
+      clearInterval(interval)
+    }
+    if(attempts > 10) {
+      console.log('Toggle Trello Extension could not find the correct place to mount :(')
+      clearInterval(interval)
+    }
+    attempts++
+  }, 1000);
 };
